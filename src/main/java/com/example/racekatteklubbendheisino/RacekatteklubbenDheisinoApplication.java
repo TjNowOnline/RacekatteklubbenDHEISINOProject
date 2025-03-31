e703cb25a1fe5fbbe45eb0bd5e80d7813457e5da
Daniel <dmastek@hotmail.com>
2025-03-31 10:52:14 +0200

    Initial commit

---
+++ src/main/java/com/example/racekatteklubbendheisino/RacekatteklubbenDheisinoApplication.java
@@ -0,0 +1,13 @@
+package com.example.racekatteklubbendheisino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RacekatteklubbenDheisinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RacekatteklubbenDheisinoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/racekatteklubbendheisino/RacekatteklubbenDheisinoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.racekatteklubbendheisino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RacekatteklubbenDheisinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
