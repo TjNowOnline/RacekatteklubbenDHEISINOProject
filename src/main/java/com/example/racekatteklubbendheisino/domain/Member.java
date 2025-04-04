5ccc41cb32b0c0d5d174bb33a909b278873e4de1
theisjensen <theis1@live.dk>
2025-04-04 10:28:56 +0200

    list

--- src/main/java/com/example/racekatteklubbendheisino/domain/Member.java
+++ src/main/java/com/example/racekatteklubbendheisino/domain/Member.java
@@ -1,16 +1,21 @@
 package com.example.racekatteklubbendheisino.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Member {
     private Long id;
     private String name;
     private String email;
     private String password;
+    private List<Pet> pets;
 
     public Member(Long id, String name, String email, String password) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.password = password;
+        this.pets = new ArrayList<>();
     }
 
     public Long getId() {
@@ -43,5 +48,13 @@ public class Member {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(List<Pet> pets) {
+        this.pets = pets;
     }
 }
