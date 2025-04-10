61b2b42d408ee7cc1e0906a8daa503d4269b88c4
TjNowOnline <dmastek@hotmail.com>
2025-04-10 12:06:39 +0200

    Delete

--- src/main/java/com/example/racekatteklubbendheisino/application/ShowRegistration.java
+++
@@ -1,4 +0,0 @@
-package com.example.racekatteklubbendheisino.application;
-
-public class ShowRegistration {
-}
--- src/main/java/com/example/racekatteklubbendheisino/presentation/ShowController.java
+++
@@ -1,4 +0,0 @@
-package com.example.racekatteklubbendheisino.presentation;
-
-public class ShowController {
-}
